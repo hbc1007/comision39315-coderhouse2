62e59cf0053a045ff528437e1e2185e272cd1f36
humberto balcazar <hbc1007@gmail.com>
2023-04-18 16:53:03 -0500

    eliminacion funcion javascript

--- js/javascripts.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function(){
-    var header = this.document.querySelector("header");
-    header.classList.toggle("abajo", this.window.scrollY > 0);
-})
-
-// let header = document.querySelector('header');
-// header.classList.toggle('down',window.scrollY > 0);
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// window.addEventListener("scroll", function(){
+//     var header = this.document.querySelector("header");
+//     header.classList.toggle("abajo", this.window.scrollY > 0);
+// })
+
+// let header = document.querySelector('header');
+// header.classList.toggle('down',window.scrollY > 0);
